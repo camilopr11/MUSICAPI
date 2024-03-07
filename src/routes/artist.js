9839e2a15287c72f327f8b10b642c359a3645b21
Camilo Pastrana <camilop.rivas@gmail.com>
2024-03-07 00:28:23 -0500

    Added basic template for artist and song routes

---
+++ src/routes/artist.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const artistSchema = require("../models/artist");
+
+const router = express.Router();
+
+// TODO: Add artist routes
+
+module.exports = router;
---
+++ src/routes/song.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const songSchema = require("../models/song");
+
+const router = express.Router();
+
+// TODO: Add song routes
+
+module.exports = router;
