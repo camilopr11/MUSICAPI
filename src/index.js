f7dc3ba3e37913bccaa9fe7a552197238d32e02f
Camilo Pastrana <camilop.rivas@gmail.com>
2024-03-06 23:23:01 -0500

    Added default route

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 9000;
 
+// Routes
+app.get("/", (req, res) => {
+    res.send("Welcome to MusicAPI");
+});
+
 // Server listening
 app.listen(port, () => console.log("Server listening on port", port));
