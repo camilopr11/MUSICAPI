4e0512660a6e2b6aecffa97bc96528b57c855262
Camilo Pastrana <camilop.rivas@gmail.com>
2024-03-06 23:41:54 -0500

    Added MongoDB Atlas connection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Settings
 const app = express();
@@ -8,6 +10,12 @@ const port = process.env.PORT || 9000;
 app.get("/", (req, res) => {
     res.send("Welcome to MusicAPI");
 });
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
 
 // Server listening
 app.listen(port, () => console.log("Server listening on port", port));
