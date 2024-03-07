e7ff1c7a4e589233fe89b60b0cc75d59dc9083d9
Camilo Pastrana <camilop.rivas@gmail.com>
2024-03-07 00:45:58 -0500

    Added create artist to router

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -3,6 +3,15 @@ const artistSchema = require("../models/artist");
 
 const router = express.Router();
 
-// TODO: Add artist routes
+// Create artist
+router.post("/artist", (req, res) => {
+    const artist = artistSchema(req.body);
+    artist
+      .save()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+
+// Todo: Get all artists,  Get artist by ID, Update artist, Delete artist
 
 module.exports = router;
