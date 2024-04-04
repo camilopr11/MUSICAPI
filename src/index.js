3c9c6941871b8232a1fd54f7b59231ce1a762592
Camilo Pastrana <camilop.rivas@gmail.com>
2024-04-04 17:02:55 -0500

    Health check with routes and database connection

--- src/index.js
+++ src/index.js
@@ -17,14 +17,43 @@ app.get("/", (req, res) => {
     res.send("Welcome to MusicAPI");
 });
 
-// Health check endpoint
-app.get('/test', (req, res) => {
-  res.status(200).json({ status: 'UP', message: 'MusicAPI TEST is running, entities: /artists /songs' });
+function getAllRoutes() {
+  const routes = [];
+  app._router.stack.forEach((middleware) => {
+      if (middleware.route) {
+          routes.push(`${Object.keys(middleware.route.methods)[0].toUpperCase()} - ${middleware.route.path}`);
+      }
+  });
+  return routes;
+}
+
+function loadRoutes() {
+  const routers = [artistRoutes, songRoutes];
+  routers.forEach((router) => {
+      router.stack.forEach((middleware) => {
+          if (middleware.route) {
+              app._router.stack.push(middleware);
+          }
+      });
+  });
+}
+
+loadRoutes();
+
+// Endpoint de health check
+app.get("/api/test", (req, res) => {
+  const isDatabaseConnected = mongoose.connection.readyState === 1;
+  const availableRoutes = getAllRoutes();
+
+  if (isDatabaseConnected) {
+      res.status(200).json({ status: 'UP', message: 'MusicAPI TEST is running', availableRoutes });
+  } else {
+      res.status(500).json({ status: 'DOWN', message: 'Database connection failed' });
+  }
 });
 
-// Health check endpoint
-app.get('/prod', (req, res) => {
-  res.status(200).json({ status: 'DOWN', message: 'ERROR: TEST ENV' });
+app.get("/api/prod", (req, res) => {
+      res.status(500).json({ status: 'ERROR', message: 'RUNNING IN TEST ENVIRONMENT' });
 });
 
 // MongoDB connection
